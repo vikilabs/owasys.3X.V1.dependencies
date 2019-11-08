337ebd96ace9e2dbd4bf81395b9251b7634597df
Vignesh Natarajan <vikiemailbox@gmail.com>
2019-11-08 20:37:14 +0100

    zlib installation script

--- src/zlib-1.2.11/vikilabs_install.sh
+++ src/zlib-1.2.11/vikilabs_install.sh
@@ -1,17 +1,117 @@
-PATH=$PATH:/opt/crosstool/arm-none-linux-gnueabi/bin
-export PATH
-export AR=/opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ar
-export AS=/opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-as
-export LD=/opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ld
-export RANLIB=/opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ranlib
-export CC=/opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-gcc
-export NM=/opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-nm
-export CXX=/opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-g++
-export LIBFFI_CFLAGS='-I/opt/crosstool/arm-none-linux-gnueabi/arm-none-linux-gnueabi/libc/usr/include/'
-export LIBFFI_LIBS='-L/opt/crosstool/arm-none-linux-gnueabi/arm-none-linux-gnueabi/libc/lib -lffi'
+
+#Relink Compilation Parameters for ARM
+
+unlink /usr/bin/gcc
+unlink /usr/bin/as
+unlink /usr/bin/ar
+unlink /usr/bin/g++
+unlink /usr/bin/nm
+unlink /usr/bin/ld
+unlink /usr/bin/ranlib
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] unlink "
+else
+    echo "[ failure ] unlink "
+    exit 1
+fi
+
+
+
+ln -s  /opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-gcc     /usr/bin/gcc
+ln -s  /opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-as      /usr/bin/as
+ln -s  /opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ar      /usr/bin/ar
+ln -s  /opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-g++     /usr/bin/g++      
+ln -s  /opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-nm      /usr/bin/nm
+ln -s  /opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ld      /usr/bin/ld
+ln -s  /opt/crosstool/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ranlib  /usr/bin/ranlib
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] softlink "
+else
+    echo "[ failure ] softlink "
+    exit 1
+fi
+
 
 
 ./configure -shared -prefix=/opt/crosstool/arm-none-linux-gnueabi/arm-none-linux-gnueabi/libc/usr
+
+
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] Configuration"
+else
+    echo "[ failure ] Configuration"
+    exit 1
+fi
+
 make
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] Make"
+else
+    echo "[ failure ] Make"
+    exit 1
+fi
+
+
+
 sudo env "PATH=$PATH" make install
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] Make Install"
+else
+    echo "[ failure ] Make Install"
+    exit 1
+fi
+
+
+
+#Relink Compilation Parameter for Intel
+unlink /usr/bin/gcc
+unlink /usr/bin/as
+unlink /usr/bin/ar
+unlink /usr/bin/g++
+unlink /usr/bin/nm
+unlink /usr/bin/ld
+unlink /usr/bin/ranlib
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] unlink "
+else
+    echo "[ failure ] unlink "
+    exit 1
+fi
+
+
+
+ln -s /usr/bin/gcc-5                      /usr/bin/gcc
+ln -s /usr/bin/x86_64-linux-gnu-as        /usr/bin/as
+ln -s /usr/bin/x86_64-linux-gnu-ar        /usr/bin/ar
+ln -s /usr/bin/g++-5                      /usr/bin/g++      
+ln -s /usr/bin/x86_64-linux-gnu-nm        /usr/bin/nm
+ln -s /usr/bin/x86_64-linux-gnu-ld        /usr/bin/ld
+ln -s /usr/bin/x86_64-linux-gnu-ranlib    /usr/bin/ranlib
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] softlink "
+else
+    echo "[ failure ] softlink "
+    exit 1
+fi
+
+echo "[ Checking Library ] libz.so"
+
+ls /opt/crosstool/arm-none-linux-gnueabi/arm-none-linux-gnueabi/libc/usr/lib/libz.so
+
+if [ $? -eq 0 ]; then
+    echo "[ success ] libz.so exist"
+    echo "[ success ] zlib installed successfully"
+else
+    echo "[ failure ] libz.so  doesn't exist"
+    echo "[ failure ] zlib installation failed"
+    exit 1
+fi
+
 
